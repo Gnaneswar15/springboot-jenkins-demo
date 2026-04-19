57c7063af3b6eb16874df207bd69b86eab0e75f0
JONNALAGADDA GNANESWAR <148674772+Gnaneswar15@users.noreply.github.com>
2026-04-19 19:08:54 +0530

    Create endpoint.java

---
+++ SpringSB/src/main/java/com/codewithme/www/endpoint.java
@@ -0,0 +1,13 @@
+package com.codewithme.www;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from Jenkins + Spring Boot!";
+    }
+}
