751c7080a5ede295b24b813dae39caf8d42ed55e
gnaneswar15 <jgnaneswar15@gmail.com>
2026-04-19 17:46:11 +0530

    Initial commit

---
+++ SpringSB/src/main/java/com/codewithme/www/SpringSbApplication.java
@@ -0,0 +1,14 @@
+package com.codewithme.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSbApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
